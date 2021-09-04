13f6864f4819a4c98a4ef5b4d6522f5909f9fbd9
ProfessorKhwarizmi <81214834+ProfessorKhwarizmi@users.noreply.github.com>
2021-09-04 12:19:04 +0300

    Add state enum

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+<<<<<<< HEAD
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private State state;
+    [SerializeField] private Place place;
+    
+
+    public enum State
+    {
+        Active,
+        Passive,
+        Entangled,
+        ReverseEntangled
+    }
+
+    public enum Place
+    {
+        Earth,
+        Qars
+    }
+=======
     public Rigidbody2D rb;
     public float movementSpeed = 5f;
 
     public enum State {Active, Passive, Entangled, ReverseEntangled}
+>>>>>>> daf7bcb49cd7fd269d4027551b8175c67093cb77
 
     Vector2 movement;
 
@@ -19,11 +41,42 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update() {
+        switch (state) {
+            case State.Active:
+                movement.x = Input.GetAxisRaw("Horizontal");
+                movement.y = Input.GetAxisRaw("Vertical");
+                movement = movement.normalized;
+                break;
+            case State.Passive:
+                break;
+            case State.Entangled:
+                movement.x = Input.GetAxisRaw("Horizontal");
+                movement.y = Input.GetAxisRaw("Vertical");
+                movement = movement.normalized;
+                break;
+            case State.ReverseEntangled:
+                if (place == Place.Earth) {
+                    movement.x = Input.GetAxisRaw("Horizontal");
+                    movement.y = Input.GetAxisRaw("Vertical");
+                    movement = movement.normalized;
+                } else {
+                    movement.x = -Input.GetAxisRaw("Horizontal");
+                    movement.y = -Input.GetAxisRaw("Vertical");
+                    movement = movement.normalized;
+                }
+                break;
+            default: break;
+        }
+        /*
+        */
+=======
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement = movement.normalized;
+>>>>>>> daf7bcb49cd7fd269d4027551b8175c67093cb77
     }
 
     void FixedUpdate()
