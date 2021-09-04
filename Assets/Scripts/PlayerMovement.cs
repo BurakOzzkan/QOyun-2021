ded6c2f76de45b36a9ea7fc6803e35d2f4c9916d
Burak Özkan <brkozkan07@gmail.com>
2021-09-04 12:34:22 +0300

    Added 2 Player Controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,34 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-<<<<<<< HEAD
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float movementSpeed = 5f;
-    [SerializeField] private State state;
-    [SerializeField] private Place place;
+    public Rigidbody2D player1Rb;
+    public Rigidbody2D player2Rb;
+    float movementSpeed = 5f;
+    Vector2 movement;
+    State state = State.Earth;
     
-
     public enum State
     {
-        Active,
-        Passive,
+        Earth,
+        Qars,
         Entangled,
         ReverseEntangled
     }
 
-    public enum Place
-    {
-        Earth,
-        Qars
-    }
-=======
-    public Rigidbody2D rb;
-    public float movementSpeed = 5f;
-
-    public enum State {Active, Passive, Entangled, ReverseEntangled}
->>>>>>> daf7bcb49cd7fd269d4027551b8175c67093cb77
-
-    Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
@@ -41,46 +27,37 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
-        switch (state) {
-            case State.Active:
-                movement.x = Input.GetAxisRaw("Horizontal");
-                movement.y = Input.GetAxisRaw("Vertical");
-                movement = movement.normalized;
-                break;
-            case State.Passive:
-                break;
-            case State.Entangled:
-                movement.x = Input.GetAxisRaw("Horizontal");
-                movement.y = Input.GetAxisRaw("Vertical");
-                movement = movement.normalized;
-                break;
-            case State.ReverseEntangled:
-                if (place == Place.Earth) {
-                    movement.x = Input.GetAxisRaw("Horizontal");
-                    movement.y = Input.GetAxisRaw("Vertical");
-                    movement = movement.normalized;
-                } else {
-                    movement.x = -Input.GetAxisRaw("Horizontal");
-                    movement.y = -Input.GetAxisRaw("Vertical");
-                    movement = movement.normalized;
-                }
-                break;
-            default: break;
-        }
-        /*
-        */
-=======
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (state == State.Earth) state = State.Qars;
+            else if (state == State.Qars) state = State.Earth;
+        }
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement = movement.normalized;
->>>>>>> daf7bcb49cd7fd269d4027551b8175c67093cb77
+
+
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        switch (state)
+        {
+            case State.Earth:
+                player1Rb.MovePosition(player1Rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+                break;
+            case State.Qars:
+                player2Rb.MovePosition(player2Rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+                break;
+            case State.Entangled:
+                break;
+            case State.ReverseEntangled:
+                break;
+            default: break;
+        }
     }
+
 }
